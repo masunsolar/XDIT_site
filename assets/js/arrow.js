4f47e6fe7aa97143173c22429f7a50b68ae6a648
masun <masunsolar@gmail.com>
2025-08-04 19:28:15 -0300

    updating

--- assets/js/arrow.js
+++ assets/js/arrow.js
@@ -1,14 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
     const scrollArrow = document.getElementById('scrollArrow');
 
-    // Verifica se o elemento da seta existe na página
     if (scrollArrow) {
         window.addEventListener('scroll', () => {
-            // A seta desaparece quando a rolagem for maior que 100 pixels
             if (window.scrollY > 5) {
-                scrollArrow.classList.add('hidden');
+                // Define a opacidade para 0
+                scrollArrow.style.opacity = '0';
             } else {
-                scrollArrow.classList.remove('hidden');
+                // Volta a opacidade para 1
+                scrollArrow.style.opacity = '1';
             }
         });
     }
